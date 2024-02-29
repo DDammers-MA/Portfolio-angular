0572db057197b95f06468add62da906148380f45
Daniel <33257@ma-web.nl>
2024-02-29 22:02:23 +0100

    minor update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, AfterViewInit, OnInit } from '@angular/core';
 import Typewriter from 't-writer.js';
 
 import { isPlatformBrowser } from '@angular/common';
@@ -12,37 +12,48 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements AfterViewInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+export class HomeComponent implements OnInit {
+  constructor(){}
 
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      setTimeout(() => {
-        const target = document.querySelector('.tw');
-        console.log('Target element:', target);
+  ngOnInit(): void {
+    const target = document.querySelector('.tw')
+    const target2 = document.querySelector('.tw2')
 
-        const writer = new Typewriter(target, {
-          loop: true,
-          typeSpeed: 80,
-          deleteSpeed: 80,
-          typeColor: 'black'
-        });
 
-        writer
-          .type("I am a Software Developer.")
-          .rest(500)
-          .changeOps({ deleteSpeed: 80 })
-          .remove(19)
-          .type('Programmer.')
-          .rest(500)
-          .remove(11)
-          .type('Front-end developer.')
-          .rest(500)
-          .changeOps({ deleteSpeed: 20 })
-          .remove(21)
-          .clear()
-          .start();
-      },500)
+    const writer1 = new Typewriter(target, {
+      typeSpeed: 60 ,
+   
+    })
+    
+    const writer2 = new Typewriter(target2, { 
+      typeSpeed: 60 
+    })
+    
+    writer1
+    .changeTypeColor('#EDEDED')
+      .type('I am a ')
+      .removeCursor()
+      .then(writer2.start.bind(writer2))
+      .start()
+    
+    writer2
+    .changeTypeColor('#DA0037')
+      .type("Software developer")
+      .rest(500)
+      .clear()
+      .changeTypeColor('#6C22A6')
+      .type("Programmer")
+      .rest(500)
+      .clear()
+      .changeTypeColor('#387ADF')
+      .type("Frond-end developer")
+      .rest(500)
+      .clear()
+      .changeTypeColor('black')
+      .then(writer1.start.bind(writer1))
+    
   }
-  }
+
+
+  
 }
